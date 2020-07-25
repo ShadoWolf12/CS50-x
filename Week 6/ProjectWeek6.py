462c6a0c7169e286db589e32cb0c19f1b89c566d
Mwebazaet <67561923+Mwebazaet@users.noreply.github.com>
2020-07-25 22:08:26 +0300

    Update ProjectWeek6.py

--- Week 6/ProjectWeek6.py
+++ Week 6/ProjectWeek6.py
@@ -1,20 +1,38 @@
-print("Hello, and welcome,to the virtual gradebook.")
-i = 1
-while i > 0:
-    print("How many student's grades would you like to store?\n")
-    numgrades= int(input())
-    if numgrades.isdigit():
-        j = 0
-        while j < numgrades:
-            students = []
-            grades = []
-            print("What's this student's name?")
-            newkid = input()
-            students.append(newkid)
-            print("What grade did they get?")
-            newgrade = int(int())
-            grades.append(newgrade)
-            j += 1
-            
+# Ethan
+class Student:
+    def __init__(self, FirstName, lastName, grade):
+        self.first_name = FirstName
+        self.last_name = lastName 
+        self.grade = grade 
+        directory = []
+   
+    print("Hello, and welcome,to the virtual gradebook")
+    while True:
+        print("Press 1 add Student, 2 view, anything else to exit.")
+        choice = int(input())
+        if choice == 1:
+            print("What's this student's First name?\n")
+            newKidfirst = input()
+            print("What's this student's last name\n")
+            newKidlast = input()
+            print("What grade did they get?\n")
+            newgrade = int(input())
+            directory.append(new Student(newKidfirst, newKidlast, newgrade))
+        
+        elif choice == 2:
+        #code for displaying grades
+        will_continue = int(input())
+        if will_continue == 1
+            print("")
+            elif will_continue == 2
+            print("Bye!!!!\n")
+            break
+        else
+            print("For your sheer stupidity, go back to the beginning.\n")
     else:
-        print("That wasn't a valid number!You scum!\n")
+        break 
+
+
+
+
+
