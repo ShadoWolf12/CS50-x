de5c774532a85711d38b9c0d92a64311eeda1c13
Ethan <67561923+Mwebazaet@users.noreply.github.com>
2020-07-26 00:16:10 +0300

    Update ProjectWeek6.py

--- Week 6/ProjectWeek6.py
+++ Week 6/ProjectWeek6.py
@@ -1,38 +1,40 @@
-# Ethan
 class Student:
     def __init__(self, FirstName, lastName, grade):
         self.first_name = FirstName
         self.last_name = lastName 
         self.grade = grade 
-        directory = []
-   
-    print("Hello, and welcome,to the virtual gradebook")
-    while True:
-        print("Press 1 add Student, 2 view, anything else to exit.")
-        choice = int(input())
-        if choice == 1:
-            print("What's this student's First name?\n")
-            newKidfirst = input()
-            print("What's this student's last name\n")
-            newKidlast = input()
-            print("What grade did they get?\n")
-            newgrade = int(input())
-            directory.append(new Student(newKidfirst, newKidlast, newgrade))
-        
-        elif choice == 2:
-        #code for displaying grades
-        will_continue = int(input())
-        if will_continue == 1
-            print("")
-            elif will_continue == 2
-            print("Bye!!!!\n")
-            break
-        else
-            print("For your sheer stupidity, go back to the beginning.\n")
+
+class Directory:
+    def __init__(self):
+        self.students_list = []
+        self.sort_by = "selection"
+
+    def add(self):
+        print("What's this student's First name?\n")
+        newKidfirst = input()
+        print("What's this student's last name\n")
+        newKidlast = input()
+        print("What grade did they get?\n")
+        newgrade = int(input())
+        self.students_list.append(Student(newKidfirst, newKidlast, newgrade))
+
+    def __str__(self):
+        string = ""
+        studentNum = 0
+        for student in range(len(self.students_list)):
+            string +=  studentNum + ")" + student.first_name + " " + student.last_name + " scored " + student.grade +"\n"
+            student += 1
+            return string
+
+container = Directory()
+print("Hello, and welcome,to the virtual gradebook\n")
+while True:
+    print("Press 1 add Student, 2 view, anything else to exit.\n")
+    choice = int(input())
+    if choice == 1:
+        container.add()
+    elif choice ==2:
+        print(container)
     else:
-        break 
-
-
-
-
-
+        print("Thank God.\n")
+        break
